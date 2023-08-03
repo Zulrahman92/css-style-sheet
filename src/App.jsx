73d9039af9af9c419b4e4f1618d24ba6b60c52cb
Zulrahman Asmurad <125336231+Zulrahman92@users.noreply.github.com>
2023-08-03 16:31:01 +0800

    add style sheet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import HelloReact from './components/HelloReact';
+
+function App() {
+  return (
+  <>
+  <HelloReact />
+  <p className="title">Hello React</p>
+  <p className="title" style={{backgroundColor: "red"}}>Hello React</p>
+
+  </>)
+}
+
+export default App
